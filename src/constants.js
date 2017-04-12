aafc0c3ccb13cfd0ea6e69c41dd42305ae3024e7
mitland <hristiqntomov@gmail.com>
2017-04-12 13:58:19 +0300

    Test

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-export const TICKET_MODAL_ERRORS = 'TICKET_MODAL_ERRORS';
-export const TICKET_STATE = {
-    PLAYING: 'playing',
-    STOPPED: 'stopped',
-    FINISHED: 'finished',
-    PAUSED: 'paused'
-};
